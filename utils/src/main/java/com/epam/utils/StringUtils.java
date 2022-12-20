35f64d248eead0d58b9b4ee14d6e8a870e632092
HelgaNovikova <helpotaki@rambler.ru>
2022-12-20 15:22:15 -0500

    maven

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,12 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if ((str.trim().length()==0) || (str.equals("null"))){
+            return false;
+        }
+        return NumberUtils.createDouble(str)>0;
     }
 }
